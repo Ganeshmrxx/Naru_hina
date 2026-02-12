ebe8cb6287e4b1c5e94ae5b33f39a62e4fc40df3
Ganesh <100681292+Ganeshmrxx@users.noreply.github.com>
2026-02-12 13:17:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1704,7 +1704,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton("𝖲𝗍𝗋𝖾𝖺𝗆", url=silent_stream),
                     InlineKeyboardButton(
                         "𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽",
-                        url=f"https://t.me/ipapkorn_v1_bot?start=file_{query.message.chat.id}_{file_id}"
+                        url=f"https://t.me/ipapkorn01_bot?start=file_{query.message.chat.id}_{file_id}")
                     )
                 ],
                 [
